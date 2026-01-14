53714a97d9b4fdf75399a7d5b0d81e136a973d05
farelft <farelft02@gmail.com>
2026-01-14 10:29:45 +0700

    add primeng primeflex and data meal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { DropdownModule } from 'primeng/dropdown';
+import { CalendarModule } from 'primeng/calendar';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
@@ -10,8 +19,20 @@ import { RecipeDetailComponent } from './components/recipe-detail/recipe-detail.
 
 @NgModule({
   declarations: [AppComponent, RecipeListComponent, RecipeDetailComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    TableModule,
+    DialogModule,
+    DropdownModule,
+    CalendarModule,
+    ToastModule,
+  ],
+  providers: [MessageService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
